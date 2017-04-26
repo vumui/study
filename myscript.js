dd72cc3c31bc338fcf05659b9d0a355983606325
vu thi mui <vu.mui@mulodo.com>
2017-04-26 20:29:48 +0900

    4/26 ＃8まで勉強しました。

---
+++ myscript.js
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang "ja">
+<head>
+
+<met charset="utf-8">
+<title>JavarScrip</title>
+</head>
+<body>
+  <script>
+var a="message";
+console.log("a");
+ </script>
+</body>
+
+</html>
